9029bedb29e5b6a465a67ece36503c48870846f2
Babithkp <babithkullachetti44@gmail.com>
2025-09-15 18:24:21 +0530

    cors edit

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,10 @@ import userRouter from "./router/user.js";
 import { jwtErrorHandler } from "./middleware/auth.js";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
